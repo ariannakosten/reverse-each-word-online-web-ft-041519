d688e0ad393f03c2074f419693a69c32cb58242f
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-21 02:49:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,6 +8,12 @@ def reverse_each_word(string)
     arr2.join
   end
   
+  def reverse_each_word(sentence)
+  reversed = sentence.split(" ").collect do |word|
+  word.reverse
+end
+  reversed.join(" ")
+end
   
   # returns that same sentence with each word reversed in place.
   #First solve it using .each Then write the same method using .collect to see the difference.
